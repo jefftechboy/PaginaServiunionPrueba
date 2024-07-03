be0235dd9c58d4bb58a31e2db943afe6e3a07afb
jefftechboy <134809393+jefftechboy@users.noreply.github.com>
2024-07-02 23:23:58 -0400

    actualizar archivos

--- MockUpPagina/settings.py
+++ MockUpPagina/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'core/static' ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
