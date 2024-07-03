45d25278125b68cdbb1055d5a6fba0dd3eb8bec9
jefftechboy <134809393+jefftechboy@users.noreply.github.com>
2024-07-02 22:19:13 -0400

    Añadir nuevos archivos al repositorio

--- MockUpPagina/settings.py
+++ MockUpPagina/settings.py
@@ -14,11 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'core/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
