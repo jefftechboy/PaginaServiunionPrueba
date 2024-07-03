8f8a35c44aca6dc00c8e1847b5edb681fa7dd21e
jefftechboy <134809393+jefftechboy@users.noreply.github.com>
2024-07-02 23:19:11 -0400

    actualizar archivos

--- MockUpPagina/wsgi.py
+++ MockUpPagina/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from settings import *
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MockUpPagina.settings')
 
 application = get_wsgi_application()
